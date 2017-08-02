2fd8c5b352cfd8b976e2cce3ad699e1dcb8f04d2
Bjt <jiantaobian@qq.com>
2017-08-02 15:02:37 +0800

    准备工作,搭建 tabbar

--- MTShop/Component/More/MTMore.js
+++ MTShop/Component/More/MTMore.js
@@ -1,3 +1,42 @@
 /**
  * Created by BJT on 17/8/2.
  */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+var More = React.createClass({
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                     更多
+                </Text>
+            </View>
+        );
+    }
+
+});
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    }
+});
+
+module.exports =  More;
