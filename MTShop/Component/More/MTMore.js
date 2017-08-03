66db3149789c60dae03ed57a741391db9caef88d
Bjt <jiantaobian@qq.com>
2017-08-03 09:52:20 +0800

    设置[更多] 界面

--- MTShop/Component/More/MTMore.js
+++ MTShop/Component/More/MTMore.js
@@ -7,35 +7,121 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
-    View
+    View,
+    Image,
+    TouchableOpacity,
+    Platform,
+    ScrollView
 } from 'react-native';
 
-var More = React.createClass({
+/**-------引入外部的组件----------**/
+var CommonCell = require('./MTCommonCell');
 
+var More = React.createClass({
+    
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                     更多
-                </Text>
+                {/*导航条*/}
+                {this.renderNavBar()}
+
+                <ScrollView>
+                    <View style={{marginTop:20}}>
+                        <CommonCell
+                            title="扫一扫"
+                        />
+                    </View>
+
+                    <View style={{marginTop:20}}>
+                        <CommonCell
+                            title="省流量模式"
+                            isSwitch={true}
+                        />
+                        <CommonCell
+                            title="消息提醒"
+                        />
+                        <CommonCell
+                            title="邀请好友使用码团"
+                        />
+                        <CommonCell
+                            title="清空缓存"
+                            rightTitle="1.99M"
+                        />
+                    </View>
+
+                    <View style={{marginTop:20}}>
+                        <CommonCell
+                            title="问卷调查"
+                        />
+                        <CommonCell
+                            title="支付帮助"
+                        />
+                        <CommonCell
+                            title="网络诊断"
+                        />
+                        <CommonCell
+                            title="关于码团"
+                        />
+                        <CommonCell
+                            title="我要应聘"
+                        />
+                    </View>
+
+                    <View style={{marginTop:20}}>
+                        <CommonCell
+                            title="精品应用"
+                        />
+                    </View>
+                </ScrollView>
             </View>
         );
+    },
+    
+    // 导航条
+    renderNavBar(){
+        return(
+            <View style={styles.navOutViewStyle}>
+                <Text style={{color:'white', fontSize:17, fontWeight:'bold'}}>更多</Text>
+                <TouchableOpacity onPress={()=>{alert('点了!')}} style={styles.rightViewStyle}>
+                    <Image source={{uri: 'icon_mine_setting'}} style={styles.navImageStyle}/>
+                </TouchableOpacity>
+            </View>
+        )
     }
 
 });
 
 
 const styles = StyleSheet.create({
+
+    navImageStyle:{
+        width:Platform.OS == 'ios' ? 28: 24,
+        height:Platform.OS == 'ios' ? 28: 24
+    },
+
+    rightViewStyle:{
+        // 绝对定位
+        position:'absolute',
+        right:10,
+        bottom:Platform.OS == 'ios' ? 10:13
+    },
+
+    navOutViewStyle:{ // 导航栏
+        height: Platform.OS == 'ios' ? 64 : 44,
+        backgroundColor:'rgba(255,96,0,1.0)',
+
+        // 设置主轴的方向
+        flexDirection:'row',
+        // 垂直居中 ---> 设置侧轴的对齐方式
+        alignItems:'center',
+        // 主轴方向居中
+        justifyContent:'center',
+        paddingTop:10
+    },
+
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10
+        backgroundColor: '#e8e8e8'
     }
 });
 
